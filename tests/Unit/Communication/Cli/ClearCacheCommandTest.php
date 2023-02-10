ef2152bc93f8b9396f1d8760698484752b76e0c4
Stanislau Komar <Stanislau_Komar@epam.com>
2023-02-11 01:23:24 +0400

    v1.6.1: Implements cache:clear command

---
+++ tests/Unit/Communication/Cli/ClearCacheCommandTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Micro\Plugin\Cache\Test\Unit\Communication\Cli;
+
+use Micro\Plugin\Cache\Communication\Cli\ClearCacheCommand;
+use PHPUnit\Framework\TestCase;
+
+class ClearCacheCommandTest extends TestCase
+{
+
+}
+
+
